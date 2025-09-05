b0241bde80c5dd25f1e26e87c384cdf0c69b75fa
Pramukh <pramukhkashyaprm@gmail.com>
2025-09-05 10:10:19 +0530

    Initial commit - Vite + React + Tailwind v4 setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-500 text-white text-2xl">
+      Vite + React + Tailwind v4 up and running!
+    </div>
+  );
+}
